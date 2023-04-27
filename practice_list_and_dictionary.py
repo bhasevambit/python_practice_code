74b2c47a5d9365ac22b7233a9b183488673a59f9
bhasevambit <jammadoibit6123@gmail.com>
2023-04-27 15:43:33 +0900

    Update several files

--- practice_list_and_dictionary.py
+++ practice_list_and_dictionary.py
@@ -16,18 +16,29 @@ class ExtractListTest(unittest.TestCase):
     # -----------------------
     # --- Unit TEST Class ---
     # -----------------------
-    def test_extract_list(self):
 
+    def test_extract_list1(self):
         test_dict_list = [
             {"name": "Taro", "age": 37},
             {"name": "Hanako", "age": 37},
             {"name": "Ken", "age": 9},
             {"name": "Dai", "age": 20},
         ]
-
-        expected_results1 = [
+        expected_results = [
             {"name": "Taro", "age": 37},
             {"name": "Hanako", "age": 37}
+        ]
+
+        actual_results = extract_list(test_dict_list, 37)
+
+        self.assertEqual(expected_results, actual_results)
+
+    def test_extract_list2(self):
+        test_dict_list = [
+            {"name": "Taro", "age": 37},
+            {"name": "Hanako", "age": 37},
+            {"name": "Ken", "age": 9},
+            {"name": "Dai", "age": 20},
         ]
         expected_results2 = [
             {"name": "Ken", "age": 9}
@@ -37,12 +48,10 @@ class ExtractListTest(unittest.TestCase):
         ]
         expected_results4 = []
 
-        actual_results1 = extract_list(test_dict_list, 37)
         actual_results2 = extract_list(test_dict_list, 9)
         actual_results3 = extract_list(test_dict_list, 20)
         actual_results4 = extract_list(test_dict_list, 1)
 
-        self.assertEqual(expected_results1, actual_results1)
         self.assertEqual(expected_results2, actual_results2)
         self.assertEqual(expected_results3, actual_results3)
         self.assertEqual(expected_results4, actual_results4)
