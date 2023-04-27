40e229017a8a8bcd050db113296c27687fa5d50d
bhasevambit <jammadoibit6123@gmail.com>
2023-04-27 15:25:43 +0900

    Add Unit Test Class

--- practice_list_and_dictionary.py
+++ practice_list_and_dictionary.py
@@ -1,12 +1,52 @@
-test_dict_list = [
-    {"name": "Taro", "age": 37},
-    {"name": "Hanako", "age": 37},
-    {"name": "Ken", "age": 9},
-    {"name": "Dai", "age": 20},
-]
+import unittest
 
-extract_list = list(
-    filter(
-        lambda item: item['age'] == 37,
-        test_dict_list))
-print(extract_list)
+
+def extract_list(test_dict_list, search_age):
+    # ----------------------------
+    # --- TEST Target Function ---
+    # ----------------------------
+    extract_list = list(
+        filter(
+            lambda item: item['age'] == search_age,
+            test_dict_list))
+    return extract_list
+
+
+class ExtractListTest(unittest.TestCase):
+    # -----------------------
+    # --- Unit TEST Class ---
+    # -----------------------
+    def test_extract_list(self):
+
+        test_dict_list = [
+            {"name": "Taro", "age": 37},
+            {"name": "Hanako", "age": 37},
+            {"name": "Ken", "age": 9},
+            {"name": "Dai", "age": 20},
+        ]
+
+        expected_results1 = [
+            {"name": "Taro", "age": 37},
+            {"name": "Hanako", "age": 37}
+        ]
+        expected_results2 = [
+            {"name": "Ken", "age": 9}
+        ]
+        expected_results3 = [
+            {"name": "Dai", "age": 20}
+        ]
+        expected_results4 = []
+
+        actual_results1 = extract_list(test_dict_list, 37)
+        actual_results2 = extract_list(test_dict_list, 9)
+        actual_results3 = extract_list(test_dict_list, 20)
+        actual_results4 = extract_list(test_dict_list, 1)
+
+        self.assertEqual(expected_results1, actual_results1)
+        self.assertEqual(expected_results2, actual_results2)
+        self.assertEqual(expected_results3, actual_results3)
+        self.assertEqual(expected_results4, actual_results4)
+
+
+if __name__ == "__main__":
+    unittest.main()
